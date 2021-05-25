c4d0fb216937740bd389a38d731b857842586865
JunlianLUO <47482896+40000mL@users.noreply.github.com>
2021-05-26 01:20:09 +0200

    Update force_external_feedback_loop.py
    Pre-training simulation added; beta set to 0.2; I saw you did nice figures for post-training, and I tried simulating post-training in real-time but it got chaotic in the later part.

--- force_external_feedback_loop.py
+++ force_external_feedback_loop.py
@@ -5,7 +5,6 @@ Created on Thu May 13 10:46:11 2021
 
 @author: zammour
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.sparse as sparse
@@ -40,7 +39,7 @@ nRec2Control = int(N/2)
 %beta = 0.1;	
 """			
 
-beta = 0.0;
+beta = 0.2;
 wo = beta*np.random.randn(nRec2Out,1)/np.sqrt(N/2);			# synaptic strengths from internal pool to output unit
 dwo = np.zeros((nRec2Out,1));
 wc = beta*np.random.randn(nRec2Control, 1)/np.sqrt(N/2);		# synaptic strengths from internal pool to control unit
@@ -61,10 +60,12 @@ print('   alpha: '+ str(alpha));
 print('   nsecs: '+ str(nsecs));
 print('   learn_every: '+ str(learn_every));
 
-
-simtime = np.arange(0, nsecs, dt)
+pre_training=np.arange(0, nsecs/2, dt);
+simtime = np.arange(0, nsecs, dt);
 simtime_len = len(simtime);
-simtime2 = np.arange(1*nsecs, 2*nsecs, dt)
+simtime2 = np.arange(1*nsecs, 2*nsecs, dt);
+post_training=np.arange(0, nsecs/2, dt);
+
 
 amp = 1.3;
 freq = 1/60;
@@ -76,6 +77,8 @@ ft2 = ft2/1.5;
 
 wo_len = np.zeros((1,simtime_len))  
 wc_len = np.zeros((1,simtime_len))
+zpre = np.zeros((1,simtime_len))
+ypre = np.zeros((1,simtime_len))
 zt = np.zeros((1,simtime_len))
 yt = np.zeros((1,simtime_len))
 zpt = np.zeros((1,simtime_len))
@@ -96,6 +99,73 @@ Pz = (1.0/alpha)*np.identity(nRec2Out);
 Py = (1.0/alpha)*np.identity(nRec2Control);
 
 
+
+#pre-training
+
+n1_pre = np.array([])
+n10_pre = np.array([])
+n50_pre = np.array([])
+n100_pre = np.array([])
+n200_pre = np.array([])
+n500_pre = np.array([])
+n600_pre = np.array([])
+n700_pre = np.array([])
+n800_pre = np.array([])
+n900_pre = np.array([])
+
+for t in simtime:				# don't want to subtract time in indices
+     
+    if ti%(nsecs/2) == 0:
+    	print('time: ' + str(t) +'.');
+      #  plt.figure('fig.training');
+    	plt.subplot(211);
+    	plt.plot(simtime, ft.T, linewidth = linewidth, color = 'green');
+    	plt.plot(simtime, zpre.T, linewidth = linewidth, color = 'red');
+    	plt.plot(simtime, ypre.T, linewidth = linewidth, color = 'magenta'); 
+    	plt.title('pre-train', fontsize = fontsize, fontweight = fontweight);
+    	plt.xlabel('time', fontsize = fontsize, fontweight = fontweight);
+    	plt.ylabel('f, z and y', fontsize = fontsize, fontweight = fontweight);
+    	plt.legend(['f', 'z', 'y']);
+    	
+    	plt.subplot(212);
+    	plt.plot(simtime, wo_len.T, linewidth = linewidth); 
+    	plt.plot(simtime, wc_len.T, linewidth = linewidth, color = 'green'); 
+    	plt.xlabel('time', fontsize = fontsize, fontweight = fontweight);
+    	plt.ylabel('|w_o|, |w_c|', fontsize = fontsize, fontweight = fontweight);
+    	plt.legend(['|w_o|', '|w_c|']);	
+    
+    	plt.pause(0.5);	
+    
+    # sim, so x(t) and r(t) are created.
+    x = (1.0-dt)*x + M*(r*dt) + wf*(y*dt); # note the y here.
+    
+    n1_pre = np.append(n1_pre, x[1])
+    n10_pre = np.append(n10_pre, x[50])
+    n50_pre = np.append(n50_pre, x[1])
+    n100_pre = np.append(n100_pre, x[100])
+    n200_pre = np.append(n200_pre, x[20])
+    n500_pre = np.append(n500_pre, x[500])
+    n600_pre = np.append(n600_pre, x[600])
+    n700_pre = np.append(n700_pre, x[700])
+    n800_pre = np.append(n800_pre, x[800])
+    n900_pre = np.append(n900_pre, x[900])
+
+    r = np.tanh(x);
+    rz = r[zidxs];			#the neurons that project to the output
+    ry = r[yidxs];			#the neurons that project to the control unit
+    z = wo.T*rz;
+    y = wc.T*ry;
+    
+    zpre.T[ti] = z;
+    ypre.T[ti] = y;
+    wo_len[0,ti] = np.sqrt(np.dot(wo.T, wo));	
+    wc_len[0,ti] = np.sqrt(np.dot(wc.T, wc));
+    ti += 1;    
+
+error_avg_pre = np.sum(np.abs(zpre-ft2))/simtime_len
+print('Pre-training MAE: ' + str(error_avg_pre))
+
+# FORCE learning
 n1_train = np.array([])
 n10_train = np.array([])
 n50_train = np.array([])
